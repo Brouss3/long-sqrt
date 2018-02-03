cc07ae4a39953a6e0197bf3e3f13a11a89a88c8f
Brouss3 <36103219+Brouss3@users.noreply.github.com>
2018-02-03 13:36:29 +0100

    Add files via upload
    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# long-sqrt
-Python proggie that computes square roots of large numbers. Algo is from me. Dunno if I reinvented something known...
+
+Goals: 
+======
+
+* Calculate square roots of large integer numbers in acceptable time.
+(Been told I was O(log(n)²) based on chronoed operations)
+
+* Create my first github repo
+
+Usage:
+======
+    
+from longSqrt import lsqrt,sqNear1
+# (sqNear1 optional, for alternate seed sqrt func)
+x=119**300+11**9999+7**53   #or any long int
+print(lsqrt(x))                  #voila
+y=lsqrt(x,sqNear1)
+print(y)
+
+
+Last word:
+==========
+
+Yes, that's short. But it's for a 20 line prog...
---
+++ longSqrt.py
@@ -0,0 +1,35 @@
+
+from math import sqrt as msqrt,log as mlog
+
+#original seed square root function
+def sqNear0(lf):
+    assert(lf>=0)
+    d=int(mlog(lf,10))
+    r0=10**((d)//2)
+    if(r0**2>lf ):
+        assert(False)
+    fl=lf*10**(2*10)//r0**2
+    fl=msqrt(fl)
+    r1=r0*int(fl*10**10)//10**20
+    return(r1)
+
+#after a while I thought I made complicated something I could do more simply
+def sqNear1(lf):
+    le=int(mlog(lf,10)//2)*2
+    if le <100:
+        return(int(msqrt(lf)))
+    a=lf//10**(le-100)
+    return(int(msqrt(a))*10**((le-100)//2))
+    
+#looping function
+def lsqrt(v,seed=sqNear0):               
+    assert(v>=0)
+    if v<1:
+        return(0L)
+    r=seed(v)    
+    r=min(r,v//r)
+    while (r+1)**2<v:
+        r+=int((v//r+1-r)/2)
+        r=min(r,v//r)
+    return(r)
+
